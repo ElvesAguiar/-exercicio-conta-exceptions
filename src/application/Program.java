c870f95229425cc5236694351444ce7411fda38e
Elves Aguiar <elvesmhs@hotmail.com>
2022-04-07 17:01:05 -0300

    Toque final

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,46 +10,47 @@ import model.exceptions.DomainException;
 public class Program {
 
 	public static void main(String[] args) {
-	Locale.setDefault(Locale.US);
-	Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-	
-	try {
-	System.out.println("Enter account data");
-	//adquirindo dados
-	
-	System.out.print("Number: ");
-	int number =sc.nextInt();
-	System.out.print("Holder: ");
-	sc.nextLine();
-	String holder = sc.nextLine();
-	System.out.print("Initial balance: ");
-	double initialBalance = sc.nextDouble();
-	System.out.print("Withdraw limit: ");
-	double witharawLimit = sc.nextDouble();
-	
-	Account account = new LendingAccount(number, holder, initialBalance, witharawLimit);
-	
-	System.out.println("");
-	//testando metodo withdraw
-	System.out.print("Enter amount for withdraw: ");
-	
-	double withdraw = sc.nextDouble();
-	if(withdraw<0) {
+		try {
+			System.out.println("Enter account data");
+			// adquirindo dados
+
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double initialBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double witharawLimit = sc.nextDouble();
+
+			Account account = new LendingAccount(number, holder, initialBalance, witharawLimit);
+
+			System.out.println("");
+			// testando metodo withdraw
+			System.out.print("Enter amount for withdraw: ");
+
+			double withdraw = sc.nextDouble();
+			if (withdraw < 0) {
+
+				throw new RuntimeException("It is not possible to withdraw negative amounts!!!");
+			}
+			account.withdraw(withdraw);
+
+			System.out.println("New balance: " + account.getBalance());
+
+		} catch (DomainException e) {
+			System.out.println(e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			sc.close();
+		}
+
 		sc.close();
-		throw new RuntimeException("It is not possible to withdraw negative amounts!!!");
-	}
-	account.withdraw(withdraw);
-	
-	System.out.println("New balance: "+account.getBalance());
-	
-	}catch (DomainException e) {
-		System.out.println(e.getMessage());
-	}catch (RuntimeException e) {
-		System.out.println(e.getMessage());
-	}
-	
-	sc.close();
 	}
 
 }
